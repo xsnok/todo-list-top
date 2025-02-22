73b18e8f60d88abc41da8173b9d393a02fac094a
Snok <chocowaco152@gmail.noreply.github.com>
2025-02-22 05:40:51 -1000

    Fix styles.cc to styles.css

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,18 @@
-import "./styles.css";
+import "../styles.css";
+
+class TodoItem {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    getDetails() {
+        return `${this.title} ${this.description} ${this.dueDate} ${this.priority}`;
+    }
+}
+
+let item = new TodoItem("title", "description", "dueDate", "priority");
+console.log(item.getDetails());
+console.log("hello world");
